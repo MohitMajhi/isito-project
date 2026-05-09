a377e8255dee537b6c4a0b9bc989b013a484f7e1
MohitMajhi <mohitmajhi99@gmail.com>
2026-05-10 03:04:46 +0530

    feat:HEAD feat: creation of a repo with fe api isito resources

---
+++ frontend/src/frontend.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def call_backend():
+    response = requests.get("http://backend-svc:5000")
+    return f"Frontend got: {response.text}"
+
+app.run(host="0.0.0.0", port=5000)
